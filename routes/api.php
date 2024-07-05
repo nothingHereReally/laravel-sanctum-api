f8351805738660d4bd7a14ff96bbb5b415eabc12
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-07-05 12:36:28 +0800

    done working on pdf task

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiResource('posts', PostController::class);
+    Route::post('comments', [CommentController::class, 'store']);
+    Route::put('comments/{comment}', [CommentController::class, '']);
+    Route::delete('comments/{comment}', [CommentController::class, 'destroy']);
+});
+
