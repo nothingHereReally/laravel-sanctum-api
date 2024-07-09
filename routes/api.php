375fea94ff80a65aec50741c70a5f0f249bb582b
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-07-09 14:08:16 +0800

    fix comment route
    missing update function be written on route

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->group(function(){
     Route::apiResource('posts', PostController::class);
     Route::post('comments', [CommentController::class, 'store']);
-    Route::put('comments/{comment}', [CommentController::class, '']);
+    Route::put('comments/{comment}', [CommentController::class, 'update']);
     Route::delete('comments/{comment}', [CommentController::class, 'destroy']);
 });
 
