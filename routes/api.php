bc3e30503667efb2332eaab8b366cea5b9e802e8
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-07-09 19:34:17 +0800

    added routes for register, login, logout
    login and register not token needed, logout token mandated

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,27 @@
 
 use App\Http\Controllers\Api\CommentController;
 use App\Http\Controllers\Api\PostController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\UserController;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::apiResource('posts', PostController::class);
     Route::post('comments', [CommentController::class, 'store']);
     Route::put('comments/{comment}', [CommentController::class, 'update']);
     Route::delete('comments/{comment}', [CommentController::class, 'destroy']);
+
+    Route::delete('user/logout/{id}', [UserController::class, 'logout']);
 });
 
+Route::group(['prefix' => 'user'], function(){
+    Route::post('register', [UserController::class, 'register']);
+    Route::post('login', [UserController::class, 'login']);
+});
 // Route::get('/blah', function(){
 //     return response()->json([
 //         'aa' => 'aa 11',
